f30a2f2131abe78ce8c4cc2d5ac1ca249f8afd7f
Michael Hopfner <mhopfner@gmx.at>
2016-08-27 13:01:07 +0200

    fix mistake in String.splitKeepTrail

--- src/extend/String.ts
+++ src/extend/String.ts
@@ -8,11 +8,12 @@ String.prototype.splitOnce = function(separator) {
 String.prototype.splitKeepTrail = function(separator, limit) {
 	var ret = []
 	var cursor = 0
-	for(var x=0;x<limit;++x) {
+	for(var x=1;x<limit;++x) {
 		var index = this.indexOf(separator, cursor)
 		if(index == -1) break
 		ret.push(this.slice(cursor, index))
 		cursor = index + 1
 	}
+	if(cursor != this.length) ret.push(this.slice(cursor))
 	return ret
 }
